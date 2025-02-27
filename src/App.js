ec488424b7f6aa3e3e8f394f592a7349c222f731
SUJINJEONG012 <peekaboo32@naver.com>
2025-02-27 21:16:39 +0900

    Feat: path 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import app from './App.css';
-import init from './styles/init.css';
-import button from './styles/button.css';
-import Main from './pages/main.js';
-import Login from './pages/login.js';
-import Signup from './pages/signup.js';
 import Detail from './pages/detail.js';
+import Login from './pages/login.js';
+import Main from './pages/main.js';
+import Signup from './pages/signup.js';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -18,6 +15,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/main" element={<Main />} />
           <Route path="/detail/:movieId" element={<Detail />} />
+          <Route path="/detail/:paymentId" element={<Detail />} />
         </Routes>
       </Router>
     </div>
