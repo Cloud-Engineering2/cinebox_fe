58173551586c506a1f34f63094e1193ee0c10ee1
hyesongChae <chs5264@naver.com>
2025-02-28 00:05:40 +0900

    add components

--- src/components/underBarTitle.js
+++ src/components/underBarTitle.js
@@ -1,19 +1,19 @@
 import { Box } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 
-const UnderBarTitle = ({title}) => {
+const UnderBarTitle = ({title, styles}) => {
 
-    const styles = {
+    const style = Object.assign({
         padding: '0px 10px 10px 10px',
         borderBottom: '2px solid #004D09',
-        margin: '0px 40px',
+        margin: title == undefined ? '30px 40px 0px 40px' : '0px 40px',
         color: '#004D09',
         fontWeight: 600,
         fontSize: 18
-    }
+    },styles)
 
     return <>
-        <Box style={styles}>
+        <Box style={style}>
             {title}
         </Box>
     </>;
