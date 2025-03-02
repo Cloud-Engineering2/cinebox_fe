c78890680e6075170f0248170d1830667def58ee
hyesongChae <chs5264@naver.com>
2025-03-03 02:47:54 +0900

    add logout, splitDateTime

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,6 +7,21 @@ export const changeTimeFormat = (datetime)=>{
     return `${date} (${hour}:${min})`;
 }
 
+export const splitDateTime = (datetime)=>{
+    const date = datetime.split('T')[0];
+    const month = date.split('-')[1];
+    const day = date.split('-')[2];
+    const time = datetime.split('T')[1].split('.')[0];
+    const hour = time.split(':')[0];
+    const min = time.split(':')[1];
+
+    return {
+        date: `${month}월 ${day}일`,
+        time: `${hour}:${min}`
+    };
+}
+
+
 export const logout = ()=>{
     localStorage.clear();
     window.location.href='/';
