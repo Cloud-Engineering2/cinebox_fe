438ffa625ec7408f94c8de5ff1bffe659e4f4a2f
hyesongChae <chs5264@naver.com>
2025-03-05 15:09:04 +0900

    admin Page - 상영정보 페이지 추가 및 user, movie 페이지 수정

--- src/pages/admin/screenTable.js
+++ src/pages/admin/screenTable.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import useReq from '../../hooks/useReq.js';
-import screenTable from '../../styles/components/screenTable.css';
-import { AppContext } from "../../App.js";
+import '../../styles/components/screenTable.css';
 import { Accordion, AccordionDetails, AccordionSummary, Alert, Box, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { splitDateTime } from '../../utils/index.js';
@@ -13,77 +12,75 @@ import ScreenForm from './screenForm.js';
 
 const ScreenTable = () => {
     const { movieId } = useParams();
-    const {context, setContext} = useContext(AppContext);
-    const [groupedScreensByDate, setGroupedScreensByDate] = useState([]);
     const [showAddScreen, setShowAddScreen] = useState(false);
-    const [showEditScreen, setShowEditScreen] = useState({screenId: null, state: false});
+    const [showEditScreen, setShowEditScreen] = useState({ info: {date: null, auditoriumId: null, screenId: null}, state: false });
 
-    const { data: getMovieScreensRes, isLoading: isGetMovieScreensLoading, error: getMovieScreensError, doRequest: doGetMovieScreensRequest } = useReq(process.env.REACT_APP_SCREEN_API, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${context.token}`
-            }
-        });
+    const { data: getMovieScreensRes, isLoading: isGetMovieScreensLoading, error: getMovieScreensError, doRequest: doGetMovieScreensRequest } = useReq(process.env.REACT_APP_MOVIE_API + `/${movieId}/screens`, {
+        method: 'GET'
+    });
     const { data: deleteMovieRes, isLoading: isDeleteMovieLoading, error: deleteMovieError, doRequest: doDeleteMovieRequest } = useReq(process.env.REACT_APP_SCREEN_API, null);
-    
-    useEffect(()=>{
-        if(getMovieScreensRes){
-            console.log(getMovieScreensRes);
-            const newScreens = groupedScreensByDate.concat(getMovieScreensRes);
-            setGroupedScreensByDate(newScreens);
-        }
-    },[getMovieScreensRes])
-    
-    const deleteScreen = (screenId)=>{
+
+
+    const deleteScreen = useCallback((screenId) => {
         doDeleteMovieRequest(process.env.REACT_APP_SCREEN_API + `/${screenId}`, {
-            method: 'DELETE',
-            headers: {
-                'Authorization': `Bearer ${context.token}`
-            }
+            method: 'DELETE'
         })
-        document.querySelector(`.screen_${screenId}`).remove();
-    }
+        document.querySelector(`#screen_${screenId}`).remove();
+    },[])
+    const getScreen = useCallback(({date, auditoriumId, screenId}) => {
+        const filteredDate = getMovieScreensRes.filter(groupedData=> groupedData.date == date);
+        const filteredAuditoriumId = filteredDate[0].auditoriums.filter(auditorium => auditorium.auditoriumId == auditoriumId);
+        const filteredScreenId = filteredAuditoriumId[0].screens.filter(screen => screen.screenId == screenId);
+
+        return filteredScreenId;
+    },[getMovieScreensRes])
 
     return <>
-        <UnderBarTitle title={'상영정보 조회'}/>
+        <UnderBarTitle title={'상영정보 조회'} />
         <Box className='screenTable'>
-            {showAddScreen && <Modal className='flex jsfy-cnt-rght mb-10' content={<ScreenForm setShowModal={setShowAddScreen} />}/>}
+            {showAddScreen && <Modal className='flex jsfy-cnt-rght mb-10' content={<ScreenForm setShowModal={setShowAddScreen} />} />}
             <Box className='flex jsfy-cnt-rght mb-10'>
-                <button id="addScreen" type="button" className="button-sm fs-23" onClick={()=>setShowAddScreen(true)}>+</button>
+                <button id="addScreen" type="button" className="button-sm fs-23" onClick={() => setShowAddScreen(true)}>+</button>
             </Box>
             {
-                groupedScreensByDate ? groupedScreensByDate.map(screen =>{
-                    return <Accordion>
-                    <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1-content"
-                    id="panel1-header"
-                    >
-                    <Typography className='screenDate' component="span">{screen.date}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                    <Box>
-                        {
-                            screen.auditoriums.map(audit=>{
-                                return <Box key={`screen_${audit.auditoriumId}`} id={`screen_${audit.auditoriumId}`} className='flex jsfy-cnt-spc-btwn mb-14'>
-                                    <Box>
-                                        <span className='startTime mr-6'>{audit.startTime && splitDateTime(audit.screens.startTime).time}</span>
-                                        <span>{`상영관 - ${audit.auditoriumId}`}</span>
-                                    </Box>
-                                    <Box>
-                                        <button id="editScreen" type="button" className="button-sm mr-6" onClick={()=>setShowEditScreen({screenId: audit.screenId, state: true})} >수정</button>
-                                        <button id="deleteScreen" type="button" className="button-sm" onClick={()=>deleteScreen(audit.auditoriumId)}>삭제</button>
-                                    </Box>
-                                </Box>
-                            })
-                        }
-                    </Box>
-                    </AccordionDetails>
+                getMovieScreensRes ? getMovieScreensRes.map(groupedData => {
+                    return <Accordion className='accordion mb-14'>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            <Typography className='screenDate' component="span">{groupedData.date}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Box>
+                                {
+                                    groupedData.auditoriums.map(auditorium => {
+                                        return <>{
+                                            auditorium.screens.map(screen => {
+                                                return <Box key={`screen_${screen.screenId}`} id={`screen_${screen.screenId}`}>
+                                                    <Box className='flex jsfy-cnt-spc-btwn mb-14'>
+                                                        <Box>
+                                                            <span className='startTime mr-6'>{`${splitDateTime(screen.startTime).time} - ${splitDateTime(screen.endTime).time}`}</span>
+                                                            <span>{screen.auditoriumName}</span>
+                                                        </Box>
+                                                        <Box>
+                                                            <button id="editScreen" type="button" className="button-sm mr-6" onClick={() => setShowEditScreen({ info: {date: groupedData.date, auditoriumId: screen.auditoriumId, screenId: screen.screenId}, state: true })} >수정</button>
+                                                            <button id="deleteScreen" type="button" className="button-sm" onClick={() => deleteScreen(screen.screenId)}>삭제</button>
+                                                        </Box>
+                                                    </Box>
+                                                </Box>;
+                                            })
+                                        }</>
+                                    })
+                                }
+                            </Box>
+                        </AccordionDetails>
                     </Accordion>
-                }) : <EmptyBox text="상영정보가 없습니다."/>
+                }) : <EmptyBox text="상영정보가 없습니다." />
             }
         </Box>
-        {showEditScreen.state && <Modal className='flex jsfy-cnt-rght mb-10' content={<ScreenForm setShowModal={setShowEditScreen} data={getMovieScreensRes.filter(dt => dt.screenId == showEditScreen.screenId)[0]}/>}/>}
+        {showEditScreen.state && <Modal className='flex jsfy-cnt-rght mb-10' content={<ScreenForm setShowModal={setShowEditScreen} data={getScreen(showEditScreen.info)[0]} />} />}
     </>
 };
 
