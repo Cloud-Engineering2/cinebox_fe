d491a1d83049523f37f9b4eb30a7dce1f5f8697b
hyesongChae <chs5264@naver.com>
2025-03-10 13:34:51 +0900

    userlist edit button

--- src/pages/admin/userList.js
+++ src/pages/admin/userList.js
@@ -1,43 +1,49 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useContext } from 'react';
 import useReq from '../../hooks/useReq.js';
 import EmptyBox from '../../components/emptyBox.js';
 import { Box } from '@mui/material';
 import UserForm from './userForm.js';
 import Modal from '../../components/modal.js';
 import '../../styles/pages/admin.css';
+import { AppContext } from "../../App.js";
 
 const UserList = () => {
+    const { context, setContext } = useContext(AppContext);
     const [showAddUser, setShowAddUser] = useState(false);
-    const [showEditUser, setShowEditUser] = useState({userId: null, state: false});
+    const [showEditUser, setShowEditUser] = useState({ userId: null, state: false });
 
     const { data, isLoading, error, doRequest } = useReq(process.env.REACT_APP_USER_API, {
         method: 'GET'
     });
     const { data: deleteMovieRes, isLoading: isDeleteMovieLoading, error: deleteMovieError, doRequest: doDeleteMovieRequest } = useReq(process.env.REACT_APP_USER_API, null);
 
-    const deleteUser = useCallback((userId)=>{
-        doDeleteMovieRequest(process.env.REACT_APP_USER_API + `/${userId}`, {
-            method: 'DELETE'
+    const deleteUser = useCallback((userId) => {
+        doDeleteMovieRequest(process.env.REACT_APP_USER_API + `/${userId}/withdraw`, {
+            method: 'POST'
         })
-        document.querySelector(`#user_${userId}`).remove();
-    },[])
+        if (userId == context.userId){
+            window.location.href='/';
+        }else{
+            document.querySelector(`#user_${userId}`).remove();
+        }
+    }, [])
 
     return <>
-        {showAddUser && <Modal className='flex jsfy-cnt-rght mb-10' content={<UserForm setShowModal={setShowAddUser} />}/>}
+        {showAddUser && <Modal className='flex jsfy-cnt-rght mb-10' content={<UserForm setShowModal={setShowAddUser} />} />}
         <Box className='flex jsfy-cnt-rght mb-10'>
-            <button id="addMovie" type="button" className="button-sm fs-23" onClick={()=>setShowAddUser(true)}>+</button>
+            <button id="addMovie" type="button" className="button-sm fs-23" onClick={() => setShowAddUser(true)}>+</button>
         </Box>        {
-            data ? data.map(user =>{
+            data ? data.map(user => {
                 return <>
-                    <Box 
-                    key={`user_${user.userId}`}
-                    id={`user_${user.userId}`}
-                    style={{
-                        marginBottom: 30,
-                        border: '2px solid #004D09',
-                        padding: 21,
-                        borderRadius: 6
-                    }}>
+                    <Box
+                        key={`user_${user.userId}`}
+                        id={`user_${user.userId}`}
+                        style={{
+                            marginBottom: 30,
+                            border: '2px solid #004D09',
+                            padding: 21,
+                            borderRadius: 6
+                        }}>
                         <Box>
                             <Box className='userCard flex'>
                                 <p className='label'>이름</p>
@@ -69,15 +75,15 @@ const UserList = () => {
                             </Box>
                         </Box>
                         <Box className='controlBox'>
-                            <button id="booking" type="button" className="button-sm mr-6" onClick={()=>window.location.href=`/admin/booking/${user.userId}`}>예매정보 조회</button>
-                            <button id="edit" type="button" className="button-sm mr-6" onClick={()=>setShowEditUser({userId: user.userId, state: true})}>수정</button>
-                            <button id="delete" type="button" className="button-sm" onClick={()=>deleteUser(user.userId)}>삭제</button>
+                            <button id="booking" type="button" className="button-sm mr-6" onClick={() => window.location.href = `/admin/booking/${user.userId}`}>예매정보 조회</button>
+                            <button id="edit" type="button" className="button-sm mr-6" onClick={() => setShowEditUser({ userId: user.userId, state: true })}>수정</button>
+                            {(user.role != 'USER' && user.userId != context.userId) && <button id="delete" type="button" className="button-sm" onClick={() => deleteUser(user.userId)}>삭제</button>}
                         </Box>
                     </Box>
                 </>
-            }) : <EmptyBox text="유저를 추가하세요."/>
+            }) : <EmptyBox text="유저를 추가하세요." />
         }
-        {showEditUser.state && <Modal className='flex jsfy-cnt-rght mb-10' content={<UserForm setShowModal={setShowEditUser} data={data.filter(dt => dt.userId == showEditUser.userId)[0]}/>}/>}
+        {showEditUser.state && <Modal className='flex jsfy-cnt-rght mb-10' content={<UserForm setShowModal={setShowEditUser} data={data.filter(dt => dt.userId == showEditUser.userId)[0]} />} />}
     </>;
 };
 
